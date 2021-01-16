c10f38785fb229dc03f84617d2b1688c05bcb779
ceponce <ceponce60@gmail.com>
2021-01-15 22:31:46 -0800

    Added bookingAPI - fixed for test

--- src/api/bookingApi.js
+++ src/api/bookingApi.js
@@ -1,21 +1,9 @@
 import { handleResponse, handleError } from "./apiUtils";
-<<<<<<< HEAD
 // const baseUrl = 'http://localhost:8089/api/v1' + "/booking/";
 const baseUrl = "https://jsonplaceholder.typicode.com/todos/1";
 
 export function getBookings() {
     return fetch(baseUrl)
         .then(handleResponse)
-=======
-const baseUrl = 'http://localhost:8089/api/v1' + "/booking/";
-
-export function getBookings() {
-    return fetch(baseUrl)
-        .then((response) => {
-            console.log("response")
-            console.log(response);
-            return response;
-        })
->>>>>>> origin/master
         .catch(handleError);
 }
